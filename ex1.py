a16601cb61b7d1babfeb14f01cf6d3ffab5bccbe
jinwu12 <jinwunc1@outlook.com>
2022-02-20 19:47:01 +0800

    https://www.runoob.com/python3/python3-type-conversion.html 数据类型转换 https://www.runoob.com/python3/python-comprehensions.html 推导式

--- ex1.py
+++ ex1.py
@@ -1,21 +1,28 @@
 #数据类型转换
+'''
+int
+float
+str
+'''
 a,b,c=1,2.7,"jinwu"
-d={}
 print(type(a))
 print(type(b))
 print(type(c))
 a=float(a)
 b=int(b)
 c1=tuple(c)
-print(type(a),'\n',type(b),'\n',type(c1))
-#推导式3
-#计算20以内的奇数
+#float int tuple
+print(type(a),type(b),type(c1))
+#推导式
+#计算20以内的奇数 
+#[1,3,5,7,9,11,13,15,17,19]
 d=[i for i in range(20) if i%2==1]
 print(d)
 #计算10以内奇数的平方数
+#{1:1,3:9,5:25,7:49,9:81}
 d1={i:i**2 for i in d if i<10}
 print(d1)
 #元组推导式
+#jn,in,nn,wn,un
 c2=(i+'n' for i in c1)
 print(tuple(c2))
-
