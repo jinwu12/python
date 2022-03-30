1ce2887ca28d8667b94ebcdb965d5813fb00ac27
jinwu12 <jinwunc1@outlook.com>
2022-03-30 17:07:54 +0800

    Combination类

--- ex26.py
+++ ex26.py
@@ -39,8 +39,11 @@ class Symbol(Model):
 
 '''
 测试案例：data
-结果：{'id': 10, 'Symbol_name': 'Char', 'comments': 'Text', 'timezone': 'Char', 'Symbol_value': 'Cha', 'contract_size': 'CharFiel', 'digits': 34, 'threepoint_price': 9978798.0, 'Baseprice': Decimal('4.00000')}
-'''
+结果：{'id': 10, 'Symbol_name': 'Char', 'comments': 'Text',
+'timezone': 'Char', 'Symbol_value': 'Cha', 'contract_size': 'CharFiel',
+'digits': 34, 'threepoint_price': 9978798.0, 'Baseprice': Decimal('4.00000')}
+
+#########################
 data = {'Symbol_name': 'Char',
         'comments': 'Text',
         'timezone': 'Char',
@@ -51,3 +54,45 @@ data = {'Symbol_name': 'Char',
         'Baseprice': 4}
 Symbol.add(data)
 Symbol.read('Char')
+'''
+
+
+class Combination(Model):
+    id = AutoField(primary_key=True)
+    Combination_name = CharField(max_length=256)
+    Combined_model = CharField(max_length=32)
+    Combination_3price = IntegerField()
+    trading_symbol = CharField(max_length=256)
+    Symbol_list = CharField(max_length=256)
+    comments = TextField()
+    Baseprice = DecimalField()
+
+    class Meta:
+        database = mydb
+        table_name = "Combination"
+
+    # 初始化方法，表不存在就创建表，并插入一条记录
+    def add(data):
+        if not Combination.table_exists():
+            Combination.create_table()
+        Combination.insert(data).execute()
+
+    # 读取表中Combination_name为“name”的数据
+    def read(name):
+        cb = Combination.get(Combination.Combination_name == name)
+        # 将cb转换成一个字典
+        i = model_to_dict(cb)
+        print(i)
+'''
+测试案例及结果
+{'id': 1, 'Combination_name': 'test', 'Combined_model': 'anyw', 
+'Combination_3price': 67, 'trading_symbol': '', 'Symbol_list': '',
+ 'comments': 'TextField()', 'Baseprice': Decimal('7.23000')}
+'''
+data = {'Combination_name': 'test',
+        'Combined_model': 'anyw',
+        'Combination_3price': 67,
+        'comments': 'TextField()',
+        'Baseprice': 7.23}
+Combination.add(data)
+Combination.read('test')
